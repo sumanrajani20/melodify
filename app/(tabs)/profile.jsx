38aa5ea909e3397cd2b3e9b153a977df6fccef52
suman rajani <sumanrajani20@gmail.com>
2024-06-15 14:30:01 +0500

    home page and sign up created

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,63 @@
+// import { router } from "expo-router";
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import { View, Image, FlatList, TouchableOpacity } from "react-native";
+
+// import { icons } from "../../constants";
+// import useAppwrite from "../../lib/appwrite";
+// import { getUserPosts, signOut } from "../../lib/appwrite";
+// import { useGlobalContext } from "../../context/GlobalProvider";
+// // import { EmptyState, InfoBox, VideoCard } from "../../components";
+
+// const Profile = () => {
+//   const { user, setUser, setIsLogged } = useGlobalContext();
+//   // const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
+//   const logout = async () => {
+//     await signOut();
+//     setUser(null);
+//     setIsLogged(false);
+
+//     router.replace("/sign-in");
+//   };
+
+//   return (
+//     <SafeAreaView className="bg-primary h-full">
+//       <View>
+//         <TouchableOpacity
+//           onPress={logout}
+//           className="flex w-full items-end mb-10"
+//         >
+//           <Image
+//             source={icons.logout}
+//             resizeMode="contain"
+//             className="w-6 h-6"
+//           />
+//         </TouchableOpacity>
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default Profile;
+// {
+//   /* <SafeAreaView className="bg-primary h-full">
+//       <TouchableOpacity
+//         onPress={logout}
+//         className="flex w-full items-end mb-10"
+//       >
+//         <Image source={icons.logout} resizeMode="contain" className="w-6 h-6" />
+//       </TouchableOpacity>
+//     </SafeAreaView> */
+// }
+import { View, Text } from "react-native";
+import React from "react";
+
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  );
+};
+
+export default profile;
