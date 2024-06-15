0a80fe6f97d13ebafc58d540fd32f76097dfe60e
suman rajani <sumanrajani20@gmail.com>
2024-06-15 19:06:25 +0500

    profile screen created

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,30 @@
-import { Iamge, Image, ScrollView, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../constants";
+
 import CustomButton from "../components/CustomButton";
+import Loader from "../components/Loader";
 import { useGlobalContext } from "../context/GlobalProvider";
 
-export default function App() {
-  const { isLoading, isLoggedIn } = useGlobalContext();
-  if (!isLoading && !isLoggedIn) return <Redirect href="/home" />;
+const Welcome = () => {
+  const { loading, isLogged } = useGlobalContext();
+
+  // if (!loading && isLogged) return <Redirect href="/home" />;
+  return <Redirect href="/home" />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
+      <Loader isLoading={loading} />
+
       <ScrollView
         contentContainerStyle={{
           height: "100%",
         }}
       >
-        <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
+        <View className="w-full flex justify-center items-center h-full px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -34,7 +41,7 @@ export default function App() {
             <Text className="text-3xl text-white font-bold text-center">
               Discover Endless{"\n"}
               Possibilities with{" "}
-              <Text className="text-secondary-200">Melodify</Text>
+              <Text className="text-secondary-200">Aora</Text>
             </Text>
 
             <Image
@@ -46,8 +53,9 @@ export default function App() {
 
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
             Where Creativity Meets Innovation: Embark on a Journey of Limitless
-            Exploration with Melodify
+            Exploration with Aora
           </Text>
+
           <CustomButton
             title="Continue with Email"
             handlePress={() => router.push("/sign-in")}
@@ -59,4 +67,6 @@ export default function App() {
       <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
-}
+};
+
+export default Welcome;
